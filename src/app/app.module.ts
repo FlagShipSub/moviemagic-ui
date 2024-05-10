94391cc110962ca6acea6428538b363bd9b03f1c
PurushothamSingh <73331484+kovelakuntlapurushotham@users.noreply.github.com>
2024-05-10 18:36:42 +0530

    added open and interceptors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HTTP_INTERCEPTORS, HttpInterceptor } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
@@ -6,11 +7,15 @@ import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { LoginModule } from './login/Login.module';
+import { AuthInterceptor } from './interceptors/auth.interceptor';
 
 @NgModule({
   declarations: [AppComponent, PageNotFoundComponent],
   imports: [BrowserModule, AppRoutingModule, LoginModule, MatSlideToggleModule],
-  providers: [provideAnimationsAsync()],
+  providers: [
+    provideAnimationsAsync(),
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
